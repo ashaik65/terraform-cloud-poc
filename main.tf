59def22eea182a2233e23befe50b176202e49255
ashaik65 <34375439+ashaik65@users.noreply.github.com>
2026-01-09 09:59:30 +0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-storage-dev"
+  location = var.location
+
+  tags = var.tags
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  enable_https_traffic_only     = true
+  min_tls_version               = "TLS1_2"
+  public_network_access_enabled = false
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = var.tags
+}
